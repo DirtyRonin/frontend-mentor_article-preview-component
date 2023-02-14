ff800496d49fbab5180bd8a8f40e06538a381e46
Maggus <mail4markus.schneider@gmx.de>
2023-02-14 11:44:44 +0100

    feat: responsive mobile design

--- src/components/article-preview.tsx
+++ src/components/article-preview.tsx
@@ -8,38 +8,39 @@ import iconPinterest from '../images/icon-pinterest.svg';
 import iconTwitter from '../images/icon-twitter.svg';
 import iconShareWhite from '../images/icon-share-white.svg';
 
+type ClassNameAttributes = 'show' | 'hide';
+
 export function ArticlePreview() {
   const [showToolTip, setToolTip] = React.useState(false);
 
+  const addAttributeOnShow = (className: string, attribute: ClassNameAttributes) => `${className}${showToolTip ? `--${attribute}` : ''}`;
+  const addAttributeOnNotShow = () => (!showToolTip ? 'hide' : '');
+
   return (
     <div className="article-preview-container">
-      <div className="preview-image-box">
-        <img alt="" src={drawers} className="preview-image" />
-      </div>
+      <img alt="" src={drawers} className="preview-image" />
       <div className="preview-text-box">
         <h1 className="preview-text-header">Shift the overall look and feel by adding these wonderful touches to furniture in your home</h1>
         <p className="preview-text-description">
           Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I’ve got some simple tips to help you make any
           room feel complete.
         </p>
-        <div className="preview-text-footer">
-          <div className="preview-text-author">
+        <div className={addAttributeOnShow('preview-text-footer', 'show')}>
+          <div className={addAttributeOnShow('preview-text-author', 'show')}>
             <img alt="" src={avatar} className="preview-text-author-image" />
             <div className="preview-text-author-details">
               <p className="preview-text-author-details-name">Michelle Appleton</p>
               <p className="preview-text-author-details-date">28 Jun 2020</p>
             </div>
           </div>
-          <div className="preview-text-social">
-            {showToolTip ? (
-              <div className="preview-text-social-tooltip">
-                <p className="preview-text-social-tooltip-label">share</p>
-                <img src={iconFacebook} alt="" className="preview-text-tooltip-facebook-icon" />
-                <img src={iconTwitter} alt="" className="preview-text-tooltip-twitter-icon" />
-                <img src={iconPinterest} alt="" className="preview-text-tooltip-pinterest-icon" />
-              </div>
-            ) : null}
-            <button className={`preview-text-social-button${showToolTip ? '--show' : ''}`} onClick={() => setToolTip((prevState) => !prevState)}>
+          <div className={addAttributeOnShow('preview-text-social', 'show')}>
+            <div className={`preview-text-social-tooltip ${addAttributeOnNotShow()}`}>
+              <p className="preview-text-social-tooltip-label">share</p>
+              <img src={iconFacebook} alt="" className="preview-text-tooltip-facebook-icon" />
+              <img src={iconTwitter} alt="" className="preview-text-tooltip-twitter-icon" />
+              <img src={iconPinterest} alt="" className="preview-text-tooltip-pinterest-icon" />
+            </div>
+            <button className={addAttributeOnShow('preview-text-social-button', 'show')} onClick={() => setToolTip((prevState) => !prevState)}>
               <img src={showToolTip ? iconShareWhite : iconShareGray} alt="" className="preview-text-social-icon" />
             </button>
           </div>
